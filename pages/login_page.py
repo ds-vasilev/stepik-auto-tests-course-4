e749a52358ab2a40ce68a8376cb2034c8854bcf3
ds-vasilev <ds_vasilev@mail.ru>
2021-10-15 15:50:49 +0300

    4-2-9 Переходы между страницами

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,7 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -21,3 +22,12 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
+
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
