1a9ac932578f4d157998db8243aea2f0381a9e9c
ds-vasilev <ds_vasilev@mail.ru>
2021-10-28 23:06:40 +0300

    4.3.2-28.10

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,10 +2,30 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ВСЕ НАХУЙ ПОМЕНЯТЬ
-
 class ProductPage(BasePage):
-    def guest_can_add_product_to_basket(self):
-        link = self.browser.find_element(*ProductPageLocators.AD_TO_BASKET)
-        self.should_be_login_url()
+
+    def add_product_to_basket(self):
+        self.sould_be_add_product_button()
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+
+    def sould_be_add_product_button(self):
+       assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "no such button"
+
+    def get_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        return book_name
+
+    def get_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        return book_price
+
+    def compare_book_name_in_basket_and_in_cataloge(self):
+        self.book_name = book_name
+        book_name_in_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET).get_attribute('textContent')
+        assert book_name == book_name_in_basket, "Book name in basket is not correct!"
+
+    def compare_book_price_in_basket_and_in_cataloge(self):
+        self.book_price = book_price
+        baskets_price = self.browser.find_element(*ProductPageLocators.BASKETS_PRICE).text
+        book_price_in_msg = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_IN_MSG).text
+        assert baskets_price == book_price, "Price in baskets is incorrect"
--- test_product_page.py
+++ test_product_page.py
@@ -2,14 +2,14 @@ from pages.product_page import ProductPage
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
-
-
-
-
-
-ВСЕ К ХУЯМ ПЕРЕПИСАТЬ
-
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
-    page.guest_can_add_product_to_basket() 
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    book_name = page.get_book_name()
+    book_price = page.get_book_price()
+    page.compare_book_name_in_basket_and_in_cataloge(book_name)
+    page.compare_book_price_in_basket_and_in_cataloge(book_price)
+
+    time.sleep(3)
